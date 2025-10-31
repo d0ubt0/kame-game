650faab7050a83244c0c890eb4b6d919850c3fd2
SergioAhOr <130256875+SergioAhOr@users.noreply.github.com>
2025-10-30 23:46:22 -0500

    Carrito: estilos del formulario

--- src/pages/Carrito.tsx
+++ src/pages/Carrito.tsx
@@ -1,13 +1,48 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+import { PageTitle } from '../components/PageTitle';
+import { PaymentForm } from './Carrito/PaymentForm';
+import './Carrito.css'
 
 export function Carrito() {
+
+  /*const [cart, setCart] = useState<Product[]>([
+    {
+      id: 1,
+      name: "Signed Japanese Blue-Eyes White Dragon",
+      price: 14000,
+      image: "/cards/blue-eyes.jpg",
+      quantity: 1,
+    },
+  ]); 
+  
+  const handleIncrease = (id: number) =>
+    setCart(cart.map(p => (p.id === id ? { ...p, quantity: p.quantity + 1 } : p)));
+
+  const handleDecrease = (id: number) =>
+    setCart(cart.map(p => (p.id === id && p.quantity > 1 ? { ...p, quantity: p.quantity - 1 } : p)));
+
+  const handleRemove = (id: number) =>
+    setCart(cart.filter(p => p.id !== id)); */
+
+
   return (
     <div>
-      <h2>Carrito</h2>
-      <p>Aquí es donde se realizan compras.</p>
-      
-      
-      <Link to="/">Volver al Inicio</Link>
+      <PageTitle title='Carrito'/>
+
+      <div className='panel-carrito'>
+        <div className='carrito'>
+          {(<p>El Carrito está vacio.</p>)}
+        </div>
+        <div className='panel-formulario'>
+          <PaymentForm />
+        </div>
+        
+      </div>
+
+      {/*
+     
+      <Link to="/">Volver al Inicio</Link>*/}
     </div>
   );
 }
---
+++ src/pages/Carrito/PaymentForm.tsx
@@ -0,0 +1,41 @@
+import './PaymentForm.css'
+
+export function PaymentForm() {
+    return(
+        <div className="formulario">
+            <div className='contenedor-titulo'>
+                <h3 className='titulo-formulario'>INFORMACIÓN DE PAGO</h3>
+            </div>
+        
+            <form className='contenedor-items'>
+                <div className='items'>
+                    <label htmlFor="">NOMBRE DEL TITULAR</label>
+                    <input type="text" />
+                </div>
+                <div className='items'>
+                    <label htmlFor="">DOCUMENTO DEL TITULAR</label>
+                    <input type="text" />
+                </div>
+                <div className='items'>
+                    <label htmlFor="">NÚMERO DE TARJETA</label>
+                    <input type="text" />
+                </div>
+                <div className='datos-tarjeta'>
+                    <div className='expiracion'>
+                        <label htmlFor="">F. EXPIRACIÓN</label>
+                        <input type="text" />
+                    </div>
+                    <div className='cvv'>
+                        <label htmlFor="">CVV</label>
+                        <input type="text" />
+                    </div>
+                </div>
+                <div className='contenedor-boton'>
+                    <button className='boton-pagar'>
+                    REALIZAR PAGO
+                </button>
+                </div>
+            </form>
+        </div>
+    )
+}
