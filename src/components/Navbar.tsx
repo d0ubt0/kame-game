79790e94532c4225e7404194b5bb43d45be57dfa
SergioAhOr <130256875+SergioAhOr@users.noreply.github.com>
2025-11-04 21:08:57 -0500

    Navbar: Iconos y Responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,25 +15,44 @@ export function Navbar() {
     <nav className="navbar">
       <ul className="navLinks">
         <div className="logo">
-          <Link to="/">Yu-Gi-Oh Store</Link>
+          <Link to="/">
+            <img src="../public/puzzle-millennium.png" alt="Logo" className="icono-logo"/>
+            <span>Yu-Gi-Oh Store</span>
+          </Link>
         </div>
 
-        <li><Link to="/Coleccion">Mi Colección</Link></li>
-        <li><Link to="/Arena">Arena de Batalla</Link></li>
-        <li><Link to="/Admin">Admin</Link></li>
-        <li><Link to="/Carrito">Carrito</Link></li>
+        <li><Link to="/Admin">
+        <img src="../public/admin.png" alt="Administrador" className="icono-admin" />
+          <span>Admin</span>
+        </Link></li>
+        <li><Link to="/Arena">
+          <img src="../public/arena.png" alt="Arena de Batalla" className="icono-arena"/>
+          <span>Arena de Batalla</span>
+        </Link></li>
+        <li><Link to="/Coleccion">
+          <img src="../public/carta.png" alt="Mi Colección" className="icono-coleccion"/>
+          <span>Mi Colección</span>
+        </Link></li>
+        <li><Link to="/Carrito">
+          <img src="../public/carrito.png" alt="Carrito" className="icono-carrito"/>
+          <span>Carrito</span>
+        </Link></li>
       </ul>
 
       <div className="authActions">
         {isAuthenticated ? (
           <>
             <span className="userName">Hola, {user?.username}</span>
-            <button onClick={handleLogout} className="authButton logoutButton">
-              Cerrar Sesión
+            <button onClick={handleLogout} className="authButton">
+              <img src="../public/logout.png" alt="Cerrar Sesion" className="icono-logout"/>
+              <span>Cerrar Sesión</span>
             </button>
           </>
         ) : (
-          <Link to="/login" className="authButton">Iniciar Sesión</Link>
+          <Link to="/login" className="authButton">
+              <img src="../public/login.png" alt="Iniciar Sesion" className="icono-login"/>
+              <span>Iniciar Sesión</span>
+            </Link>
         )}
       </div>
     </nav>
